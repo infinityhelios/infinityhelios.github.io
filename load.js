bc86e6553326edd95db8e409c435577333aaf490
infinityhelios <117550381+infinityhelios@users.noreply.github.com>
2022-11-17 20:07:02 -0500

    Initial commit

---
+++ load.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+    fetch('https://infinityhelios.neocities.org/sidenav.html')
+    .then(data => {
+      return data.text()
+    })
+    .then( data => {
+      document.getElementById("sidenav").innerHTML = data;
+    })
+}
+
+let mybutton = document.getElementById("scroll");
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
